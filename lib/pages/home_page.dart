5c5264f33daf785aa699c07c0e5e839ac188d70f
ToshevaDurdona <toshevadurdona2002@gmail.com>
2024-03-16 06:59:41 +0500

    Delet And Creat  add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:http/http.dart' as http;
+import 'package:ngdemo13/pages/creat_posts.dart';
+import 'package:ngdemo13/pages/update_post_page.dart';
 import '../models/post_model.dart';
 import '../services/http_service.dart';
 import '../services/log_service.dart';
@@ -12,51 +15,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String data = "no data";
+  bool isLoading = true;
+  List<Post> posts = [];
+
+  _loadPosts() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    var response = await Network.GET(Network.API_POST_LIST, Network.paramsEmpty());
+    LogService.d(response!);
+    List<Post> postList = Network.parsePostList(response!);
+
+    setState(() {
+      posts = postList;
+      isLoading = false;
+    });
+  }
+
+  _deletePost(Post post) async {
+    setState(() {
+      isLoading = true;
+    });
+    var response = await Network.DEL(Network.API_POST_DELETE + post.id.toString(), Network.paramsEmpty());
+    LogService.d(response!);
+    _loadPosts();
+  }
+
+  Future _callCreatePage() async {
+    bool result = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return CreatPage();
+    }));
+
+    if (result) {
+      _loadPosts();
+    }
+  }
+
+  Future _callUpdatePage(Post post) async {
+    bool result = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return UpdatePage(post: post);
+    }));
+
+    if (result) {
+      _loadPosts();
+    }
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    loadPosts();
-    //createPost();
-    //updatePost();
-    //deletePost();
+    _loadPosts();
   }
 
-  loadPosts() async {
-    var response = await Network.GET(Network.API_POST_LIST, {});
-    LogService.d(response!);
-    setState(() {
-      data = response;
-    });
-  }
-
-  createPost() async {
-    Post post = Post(title: "Nextgen", body: "Academy", userId: 1);
-    var response = await Network.POST(Network.API_POST_CREATE, Network.paramsCreate(post));
-    LogService.d(response!);
-    setState(() {
-      data = response;
-    });
-  }
-
-  updatePost() async {
-    Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
-    var response = await Network.PUT(Network.API_POST_UPDATE + post.id.toString(), Network.paramsUpdate(post));
-    LogService.d(response!);
-    setState(() {
-      data = response;
-    });
-  }
-
-  deletePost() async {
-    Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
-    var response = await Network.DEL(Network.API_POST_DELETE + post.id.toString(), Network.paramsEmpty());
-    LogService.d(response!);
-    setState(() {
-      data = response;
-    });
+  Future<void> _handleRefresh() async {
+    _loadPosts();
   }
 
   @override
@@ -66,9 +82,84 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.blue,
         title: Text("Networking"),
       ),
-      body: Center(
-        child: Text(data, style: TextStyle(fontSize: 20),),
+      body: Stack(
+        children: [
+          RefreshIndicator(
+            onRefresh: _handleRefresh,
+            child: ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (ctx, index) {
+                return _itemOfPost(posts[index]);
+              },
+            ),
+          ),
+          isLoading
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : SizedBox.shrink(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          _callCreatePage();
+        },
       ),
     );
+  }
+
+  Widget _itemOfPost(Post post) {
+    return Slidable(
+        startActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (BuildContext context) {
+                _callUpdatePage(post);
+              },
+              backgroundColor: Colors.orange,
+              foregroundColor: Colors.white,
+              icon: Icons.edit,
+              label: 'Edit',
+            ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (BuildContext context) {
+                _deletePost(post);
+              },
+              backgroundColor: Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.title!,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(post.body!,
+                  style:
+                  TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+              Divider(),
+            ],
+          ),
+        ));
   }
 }
